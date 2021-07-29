42702f3d14b4a704ecd57b68423b72871d36e637
Anton <golubin1990@gmail.com>
2021-07-29 15:27:56 -0400

    Added refactored code which splits breedFetcher.js function in two

--- fetchBreedDescription.js
+++ fetchBreedDescription.js
@@ -1,24 +1,27 @@
 const request = require('request');
 
-const requestCat = function() {
+
+const fetchBreedDescription = function(CAT, processinput) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${CAT}`, (error, response, body) => {
+
     if (error) {
-      return console.log(`There was an error in your request. Error description: ${error}`);
+      processinput(error, null)
     }
 
     if (body.length > 2) {
 
-      console.log(response.statusCode);
-      let data = body.substring(1, body.length - 1);
-      let JSONobj = JSON.parse(data);
-      console.log(JSONobj.description);
+      let input = body.substring(1, body.length - 1);
+      let JSONobj = JSON.parse(input);
+      processinput(null, JSONobj.description);
 
     } else {
 
-      return console.log(`The breed "${CAT.toUpperCase()}" that you specified was not found in our database`);
+      processinput(`The breed "${CAT.toUpperCase()}" that you specified was not found in our database`, null);
 
     }
   });
   
 };
+
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,19 @@
+const CAT = process.argv[2];
+const fetchBreedDescription = require('./fetchBreedDescription')
+
+
+const processinput = function (error, data) {
+
+  if (error) {
+
+    console.log(`Error fetch details: ${error}`);
+
+  } else {
+
+    console.log(data)
+  
+  }
+  
+};
+
+fetchBreedDescription(CAT, processinput);
