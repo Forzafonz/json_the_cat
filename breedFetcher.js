7b9eddc15cc7ed8b3e60234950ff3dbbfeb6d3c1
Anton <golubin1990@gmail.com>
2021-07-29 15:05:34 -0400

    Some minor linting changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,20 @@
 const request = require('request');
-const {stdin, stdout} = require('process');
 const CAT = process.argv[2];
 
 
-const requestCat = function (){
+const requestCat = function() {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${CAT}`, (error, response, body) => {
     if (error) {
       return console.log(`There was an error in your request. Error description: ${error}`);
     }
 
-    if (body.length > 2){
+    if (body.length > 2) {
 
       console.log(response.statusCode);
       let data = body.substring(1, body.length - 1);
       let JSONobj = JSON.parse(data);
-      console.log(JSONobj.description)
+      console.log(JSONobj.description);
 
     } else {
 
@@ -24,6 +23,6 @@ const requestCat = function (){
     }
   });
   
-}
+};
 
-requestCat()
+requestCat();
