f2ce2e760bd89c3c69ae9a025e50dd5e3ef9e0b9
Anton <golubin1990@gmail.com>
2021-07-29 15:19:02 -0400

    Refactored function to inlcude callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,27 +2,43 @@ const request = require('request');
 const CAT = process.argv[2];
 
 
-const requestCat = function() {
+const processinput = function (error, data) {
+
+  if (error) {
+
+    console.log(`There was an error in your request. Error description: ${error}`);
+
+  } else {
+
+    let input = data.substring(1, data.length - 1);
+    let JSONobj = JSON.parse(input);
+    console.log(JSONobj.description);
+  
+  }
+  
+}
+
+const fetchBreedDescription = function(CAT, processinput) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${CAT}`, (error, response, body) => {
+
     if (error) {
-      return console.log(`There was an error in your request. Error description: ${error}`);
+      processinput(error, null)
     }
 
     if (body.length > 2) {
 
-      console.log(response.statusCode);
-      let data = body.substring(1, body.length - 1);
-      let JSONobj = JSON.parse(data);
-      console.log(JSONobj.description);
+      processinput(null, body);
 
     } else {
 
-      return console.log(`The breed "${CAT.toUpperCase()}" that you specified was not found in our database`);
+      processinput(`The breed "${CAT.toUpperCase()}" that you specified was not found in our database`, null);
 
     }
   });
   
 };
 
-requestCat();
+
+
+fetchBreedDescription(CAT, processinput);
---
+++ fetchBreedDescription.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+const requestCat = function() {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${CAT}`, (error, response, body) => {
+    if (error) {
+      return console.log(`There was an error in your request. Error description: ${error}`);
+    }
+
+    if (body.length > 2) {
+
+      console.log(response.statusCode);
+      let data = body.substring(1, body.length - 1);
+      let JSONobj = JSON.parse(data);
+      console.log(JSONobj.description);
+
+    } else {
+
+      return console.log(`The breed "${CAT.toUpperCase()}" that you specified was not found in our database`);
+
+    }
+  });
+  
+};
